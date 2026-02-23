a3ab70ea9ea7eee6d22d62e736cadc2b64357eb9
avi0446 <avinash.pathania@shinedezign.com>
2026-02-23 18:16:06 +0530

    add component class only

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,31 @@
 @extends('layouts.app')
 @section('content')
-<h1>Welocome ({{ auth()->user()->name }})</h1>
-<h3>Your role is: {{ auth()->user()->getRoleNames()->first() }}</h3>
-<form action="{{ route('logout') }}" method="POST">
-    @csrf
-    <button type="submit" class="btn btn-danger">
-        Logout
-    </button>
-</form>
-<a href="{{ route('profile') }}" class="btn btn-primary mt-3">
-    View Profile
-</a>
-<a href="{{ route('admin.users') }}" class="btn btn-primary mt-3">
-    View Users
-</a>
-@endsection
+    <div class="admin-container">
+        <div class="heading-section">
+            <h1>Welocome ({{ auth()->user()->name }})</h1>
+            <h3>Your role is: {{ auth()->user()->getRoleNames()->first() }}</h3>
+        </div>
+        <div class="form-container">
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <div class="logout-section">
+                    <button type="submit" class="btn btn-danger">
+                        Logout
+                    </button>
+                </div>
+            </form>
+        </div>
+        <div class="button-sections">
+            <div class="profile-link">
+                <a href="{{ route('profile') }}" class="btn btn-primary mt-3">
+                    View Profile
+                </a>
+            </div>
+            <div class="user-section">
+                <a href="{{ route('admin.users') }}" class="btn btn-primary mt-3">
+                    View Users
+                </a>
+            </div>
+            <div>
+            </div>
+        @endsection
